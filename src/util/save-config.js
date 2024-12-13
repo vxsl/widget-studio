ef3329b1e1a919cd0f3888c6d1bd7c6df165180e
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-12-13 12:57:05 -0800

    fix file save

--- src/util/save-config.js
+++ src/util/save-config.js
@@ -1,5 +1,7 @@
+import { saveAs } from 'file-saver'
+
+
 const saveConfig = (config, title) => {
-  const { saveAs } = require('file-saver')
   var blob = new Blob([JSON.stringify(config)], { type: 'application/json' })
   saveAs(blob, `${title}.json`)
 }
